0ec5a4f9d071b5ca9e868746be1b781ddb7cedae
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-12-22 18:46:10 -0500

    fix: comments removed

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -13,25 +13,18 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
 
-// Max Binary Heap
 typedef struct binary_tree_s heap_t;
-typedef struct binary_tree_s binary_tree_t;
 
-// Print function
 void binary_tree_print(const binary_tree_t *);
 
-// New node
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
-// Heap - Insert
-heap_t *heap_insert(heap_t **root, int value);
-
-#endif /* BINARY_TREES_H */
+#endif /* _BINARY_TREES_H_ */
