2b798cad1391db47375567ffa2b71a424cd5e00f
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-12-22 18:19:22 -0500

    fix: using typedef for binary_tree_t

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -13,16 +13,17 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-} binary_tree_t;
+};
 
 // Max Binary Heap
 typedef struct binary_tree_s heap_t;
+typedef struct binary_tree_s binary_tree_t;
 
 // Print function
 void binary_tree_print(const heap_t *);
