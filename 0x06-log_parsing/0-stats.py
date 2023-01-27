f948b556094e3ae885a4ecd144b284e07c6230c5
Johanna Alfonso <jonna.alfonso@gmail.com>
2023-01-27 12:44:07 -0500

    fix: case long log

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -15,15 +15,14 @@ def main():
     try:
         for index, input in enumerate(sys.stdin, start=1):
             line = check_line_format(input.split())
-
-            if line:
-                count_line(line)
-                file_size += int(line.file_size)
-
-                if index % 10 == 0:
-                    print_metrics(file_size)
-            else:
+            if line is None:
                 continue
+
+            count_line(line)
+            file_size += int(line.file_size)
+
+            if index % 10 == 0:
+                print_metrics(file_size)
 
     finally:
         print_metrics(file_size)
