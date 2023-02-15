f081fae9596e9a120d6305226792d3be7dd4b747
Johanna Alfonso <jonna.alfonso@gmail.com>
2023-02-15 11:03:08 -0500

    feat: utf-8 validation

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Module that determines if a given data set represents a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """Determines if a given data set represents a valid UTF-8 encoding.
+
+    :param data: list of integers
+    :return: True if data is a valid UTF-8 encoding, else return False
+    """
+
+    bits = 0
+
+    for n in data:
+        n_byte = format(n, "#010b")[-8:]
+
+        if bits != 0:
+            bits -= 1
+            if not n_byte.startswith("10"):
+                return False
+
+        if n_byte.startswith("1"):
+            bits = len(n_byte.split("0")[0])
+
+    return True
